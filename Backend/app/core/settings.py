884dcaff3fc775655f3d82549d7a40174f270592
LuisaBuso <luisafbs3@gmail.com>
2025-04-24 15:01:07 -0500

    Agents folder added

--- Backend/app/core/settings.py
+++ Backend/app/core/settings.py
@@ -5,7 +5,7 @@ import os
 from app.database.mongo import connect_to_mongo
 from app.auth.routes import router as auth_router
 from app.manychat.routes import router as manychat_router
-
+from app.agents.upload import router as upload_router
 # Cargar variables de entorno
 load_dotenv()
 
@@ -18,3 +18,5 @@ connect_to_mongo()
 # Incluir rutas
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(manychat_router, prefix="/manychat", tags=["Manychat"])
+app.include_router(upload_router)
+
